07dd528042efeac8cff92f5250198f3a76430f52
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 10:05:36 +0530

    Last Digit of Number

---
+++ FCP006.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class FCP006{
+  public static void main (String args[]){
+ Scanner D = new Scanner (System.in);
+int num = D.nextInt();
+int Lnum = (num%10);
+System.out.println(Lnum);
+}}
