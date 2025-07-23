df7eaeb2a6dab8bcd5ee86fabc5d2f4a541f8294
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 10:45:46 +0530

    Largest Divisible Less Than A

---
+++ FCP009.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FCP009{
+ public static void main (String args[]){
+Scanner D = new Scanner ( System.in);
+int a = D.nextInt();
+int b = D.nextInt();
+int c = (a % b);
+int d = (a - c);
+System.out.println(d);
+}}
+
+
+ 
+
+
