b835510626ddfd39ed1894e94712d5e208bdbce2
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 12:28:55 +0530

    Smallest Multiple of 2 & n

---
+++ FCP013.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ public class FCP013{
+  public static void main (String args []){
+Scanner D = new Scanner (System.in);
+ int a = D.nextInt();
+if( a % 2 == 0 ){
+System.out.println(a);}
+else{
+System.out.println(a*2);
+}
+}}
