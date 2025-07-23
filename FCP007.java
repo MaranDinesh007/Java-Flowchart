5963157eb6f049c7639c52a85c85f366e0be8445
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 10:40:07 +0530

    Celsius to Fahrenheit Conversion

---
+++ FCP007.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class FCP007{
+ public static void main (String args[]){
+Scanner D = new Scanner (System.in);
+int c=D.nextInt();
+int f = (c*9/5)+32;
+System.out.println(f);
+}}
+
+
+
+
+
+
+
+ 
