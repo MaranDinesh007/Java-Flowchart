4c856b6ac688a04e920af057a41131195e0ef738
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 12:28:10 +0530

    Positive, Negative or Zero

---
+++ FCP012.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class FCP012{
+ public static void main (String args[]){
+Scanner D = new Scanner (System.in);
+int num = D.nextInt();
+if(num == 0){
+System.out.println("Number is Zero");
+}
+else if(num > 0 ){
+System.out.println("Number is positive");
+}
+else{
+System.out.println("Number is negative");
+}
+}}
+
