2067ee7001a070bb4b930a4303d7e3185e19aa5c
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 10:46:40 +0530

    max of two numbers

---
+++ FCP010.java
@@ -0,0 +1,13 @@
+import java.util.Scanner ;
+ public class FCP010{
+  public static void main ( String args[]){
+Scanner D = new Scanner (System.in);
+int a = D.nextInt();
+int b = D.nextInt();
+if(a>b){
+System.out.println(a);
+}
+else{
+System.out.println(b);
+}
+}}
