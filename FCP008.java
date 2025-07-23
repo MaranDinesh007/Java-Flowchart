07affba1cceea592352552b0ef2d61d55e9a9acd
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 10:44:03 +0530

    Swap Without Temp

---
+++ FCP008.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class FCP008{
+ public static void main (String args[]){
+Scanner D = new Scanner (System.in);
+int a = D.nextInt();
+int b = D.nextInt();
+ a = (a+b); 
+ b = (a-b);
+ a = (a-b);
+System.out.println(a);
+System.out.println(b);
+}}
+
