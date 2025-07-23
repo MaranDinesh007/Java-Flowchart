10c6718220bb9f02c04fd0d6523151e46bc5f15c
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 09:17:45 +0530

    Swap with Temp Variable

---
+++ FCP003.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+ public class FCP003{
+   public static void main (String a []){
+    Scanner D = new Scanner (System.in);
+
+int num1 = D.nextInt();
+int num2 = D.nextInt();
+int temp = num1;
+System.out.println(num2);
+System.out.println(temp);
+}}
