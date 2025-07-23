9f07f86b967bde7697573275e4e6beb4717aca82
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 09:16:21 +0530

    Print User Input

---
+++ FCP002.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+ public class FCP002{
+   public static void main (String a []){
+    Scanner D = new Scanner(System.in);
+System.out.println("Enter the number");
+int num1 = D.nextInt();
+System.out.print(num1);
+}}
