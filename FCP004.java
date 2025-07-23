fd05e08c551aa1993d7210462be9c3ed5cf28faf
MaranDinesh007 <dinesh.2508@xcelevate.org>
2025-07-23 12:25:56 +0530

    Basic Arithmatic Operators

---
+++ FCP004.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ public class FCP004{
+public static void main (String a[]){
+  Scanner D = new Scanner (System.in);
+    int num1 = D.nextInt();
+    int num2 = D.nextInt();
+System.out.println(num1 + num2);
+System.out.println(num1 - num2);
+System.out.println(num1 * num2);
+System.out.println(num1 / num2);
+System.out.println((double) num1 / num2);
+System.out.println(num1 % num2);
+}}
